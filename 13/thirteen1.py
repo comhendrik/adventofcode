a9db145209e3d733727e938f102e117c05cb3b36
comhendrik <hendrik0807@icloud.com>
2021-12-13 17:00:13 +0100

    day 13

---
+++ 13/thirteen1.py
@@ -0,0 +1,73 @@
+x = []
+y = []
+coordinates = []
+fold_instructions = []
+def do_y_fold(fold):
+    for i in range(len(paper[fold])):
+        paper[fold][i] = 9
+    fold_along_y(fold-1,fold+1)
+    for i in range(fold,len(paper)):
+        del paper[-1]
+
+
+def do_x_fold(fold):
+    for i in range(len(paper)):
+        paper[i][fold] = 9
+    fold_along_x(fold-1,fold+1)
+    for row in paper:
+        for _ in range(fold,len(row)):
+            del row[-1]
+
+
+def fold_along_y(newaxis,oldaxis):
+    if newaxis >= 0 and oldaxis < len(paper):
+        for pos in range(len(paper[oldaxis])):
+            if paper[oldaxis][pos] == 1:
+                paper[newaxis][pos] = paper[oldaxis][pos]
+        fold_along_y(newaxis=newaxis-1,oldaxis=oldaxis+1)
+
+
+def fold_along_x(newaxis,oldaxis):
+    if newaxis >= 0 and oldaxis < len(paper[0]):
+        for pos in range(len(paper)):
+            if paper[pos][oldaxis] == 1:
+                paper[pos][newaxis] = paper[pos][oldaxis]
+        fold_along_x(newaxis=newaxis-1,oldaxis=oldaxis+1)
+
+for line in open('13/thirteen.txt'):
+    if line != '\n' and line.startswith('fold') == False:
+        x_axis,y_axis = line.strip().split(',')
+        x.append(int(x_axis))
+        y.append(int(y_axis))
+        coordinates.append((int(x_axis),int(y_axis)))
+    else:
+        line = line.strip().split(' ')
+        if len(line) > 1:
+            line = line[2].split('=')
+            fold_instructions.append((line[0],line[1]))
+print(fold_instructions)
+#create transparent paper(2d array)
+paper = list_of_lists = [[0 for row in range(max(x)+1)] for col in range(max(y)+1)]
+#fill transparent paper with values
+for value in coordinates:
+    x,y = value
+    paper[y][x] = 1
+
+for key,value in fold_instructions:
+    if key == 'y':
+        do_y_fold(int(value))
+    else:
+        do_x_fold(int(value))
+    break
+    #we just need one instruction so we break after it.
+    
+
+for line in paper:
+    print(line)
+
+index = 0
+for line in paper:
+    for number in line:
+        index += number
+print(index)
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # adventofcode
-My Solutions for advent of code 2021.
-All written in Python
+My Solutions for advent of code 2021. 
+All written in Python. 
+Maybe these are not the best solutions but they work!
