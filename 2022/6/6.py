1866fc02403b433fb5e79edf23b9134650781fe0
comhendrik <hendrik0807@icloud.com>
2022-12-06 14:19:21 +0100

    2022 6

---
+++ 2022/6/6.py
@@ -0,0 +1,21 @@
+data = open("2022/6/input.txt", 'r')
+data = data.read().strip()
+i = 0
+while True:
+    packet = set(data[i:i+4])
+    if len(packet) == 4:
+        break
+    i += 1
+
+print("Part one")    
+print(i+4)
+
+
+while True:
+    packet = set(data[i:i+14])
+    if len(packet) == 14:
+        break
+    i += 1
+
+print("Part two")    
+print(i+14)
