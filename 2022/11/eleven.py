e2387d10608fbadd461efeb5562c28ce9f77f37e
comhendrik <hendrik0807@icloud.com>
2022-12-16 13:37:55 +0100

    2022 11 part one

--- 2022/11/eleven.py
+++ 2022/11/eleven.py
@@ -1,40 +1,46 @@
-def calculate(a,b,operator):
-    if operator == '*':
-        return a*b
-    else:
-        return a+b
+#with help from "W.Y.Feng"
+def calculate(operation, x):
+    left, right, op = operation
 
+    assert left == "old"
+
+    if op == "+":
+        return x + int(right)
+    else:
+        if right == "old":
+            return x * x
+        else:
+            return x * int(right)
 
 class Monkey:
-    def __init__(self, operation, a, b, test):
+    def __init__(self, operation, test):
         self.items = []
         self.operation = operation
-        self.a = a
-        self.b = b
         self.test = test
         self.throw_to_when_true = 0
         self.throw_to_when_false = 0
+        self.inspections = 0
+    def __str__(self):
+        return f"{self.items}, {self.operation}, {self.test}, {self.throw_to_when_true}, {self.throw_to_when_false}"
 
 monkeys = []
 #get data from monkeys into array of objects
 i = -1
 
-for line in open('2022/11/t.txt'):
+for line in open('2022/11/input.txt'):
     line = line.strip().split()
     if len(line) == 0:
         continue
     if line[0] == 'Monkey':
         i += 1
-        monkeys.append(Monkey('*','old','old',0))
+        monkeys.append(Monkey([],0))
         continue
     elif line[0] == 'Starting':
         for j in range (2,len(line)):
             number = line[j][0] + line[j][1] #Just need two digits numbers because input doesnt uses three digit numbers
             monkeys[i].items.append(int(number))
     elif line[0] == 'Operation:':
-        monkeys[i].a = line[3]
-        monkeys[i].operation = line[4]
-        monkeys[i].b = line[5]
+        monkeys[i].operation = [line[3],line[5], line[4]]
     elif line[0] == 'Test:':
         monkeys[i].test = int(line[3])
     elif line[0] == 'If':
@@ -42,31 +48,34 @@ for line in open('2022/11/t.txt'):
             monkeys[i].throw_to_when_true = int(line[5]) 
         else:
             monkeys[i].throw_to_when_false = int(line[5]) 
+            
 
-def go_trough_monkeys():
-    for monkey in monkeys:
-        print(monkey.items)
-        for i in range(len(monkey.items)):
-            if monkey.a == 'old' and monkey.b == 'old':
-                a,b = int(monkey.items[i]), int(monkey.items[i])
-            elif monkey.a == 'old':
-                a,b = int(monkey.items[i]), int(monkey.b)
-            elif monkey.b == 'old':
-                a,b = int(monkey.a), int(monkey.items[i])
-            new = calculate(a,b,monkey.operation)
-            monkey.items[i] = int(new)
-            number_to_throw = int(monkey.items[i]/3)
-            if monkey.items[i] % monkey.test == 0:
+for _ in range(20):
+    for i in range(len(monkeys)):
+        for item in monkeys[i].items:
+            number_to_throw = calculate(monkeys[i].operation, item)
+            number_to_throw = int(number_to_throw/3)
+            test = monkeys[i].test
+            if number_to_throw % test == 0:
 
-                monkeys[monkey.throw_to_when_true].items.append(number_to_throw)
+                monkeys[monkeys[i].throw_to_when_true].items.append(number_to_throw)
             else:
-                monkeys[monkey.throw_to_when_false].items.append(number_to_throw)
-        monkey.items = []
+                monkeys[monkeys[i].throw_to_when_false].items.append(number_to_throw)
 
-for i in range(2):
-    go_trough_monkeys()
-    print(monkeys[0].items,monkeys[1].items,monkeys[2].items,monkeys[3].items)
+            monkeys[i].inspections += 1
+        monkeys[i].items = []
 
+
+
+
+
+
+
+print("Part one:")
+#with help from "W.Y.Feng"
+inspections = [m.inspections for m in monkeys]
+sorted_inspections = sorted(inspections)
+print(sorted_inspections[-1] * sorted_inspections[-2])
 
     
 
