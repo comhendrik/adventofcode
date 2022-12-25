514e14b148780083ffa276d238cccf3a8cf3b86a
comhendrik <hendrik0807@icloud.com>
2022-12-25 19:25:55 +0100

    day 20 as problem for understanding code of others

--- 2022/20/twenty.py
+++ 2022/20/twenty.py
@@ -1,109 +1,51 @@
 class Node:
-    def __init__(self, swapped=False, data=None,prev=None,next=None):
-        self.data = data
-        self.next = next
-        self.prev = prev
-        self.swapped = swapped
+    def __init__(self, n):
+        self.n = n
+        self.left = None
+        self.right = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-    def append(self, data):
-        if self.head == None:
-            self.head = Node(data=data,prev=None,next=None)
-            return
-        curr = self.head
-        while curr.next != None:
-            curr = curr.next
-        curr.next = Node(data=data,prev=curr,next=None)
-    def loop(self):
-        curr = self.head
-        while curr != None:
-            self.tail = curr
-            curr = curr.next  
-    def print_list(self):
-        curr = self.head
-        while curr != None:
-            print(curr.data)
-            curr = curr.next
+x = [Node(int(x)) for x in open('2022/20/t.txt')]
 
-    def insert_element(self,prev_node,new_node,next_node):
-        new_node.prev = prev_node
-        new_node.next = next_node
-        if prev_node == None:
-            self.head = new_node
-        else:
-            prev_node.next = new_node
-        
-        if next_node == None:
-            self.tail = new_node
-        else:
-            next_node.prev = new_node
-        
+for i in range(len(x)):
+    x[i].right = x[(i + 1) % len(x)]
+    x[i].left = x[(i - 1) % len(x)]
 
-    def insert_element_after_index(self,old_node,new_node):
-        #assume i is positiv
-        curr = old_node
-        i = old_node.data
-        if i > 0:
-            for _ in range(0,i):
-                if curr.next == None:
-                    curr = self.head
-                else:
-                    curr = curr.next
-                
-                
-            if curr == None:
-                self.insert_element(prev_node=curr,new_node=new_node,next_node=self.head)
-                return
-            self.insert_element(prev_node=curr,new_node=new_node,next_node=curr.next)
-        elif i < 0:
-            for _ in range(0,abs(i)+1):
-                
-                if curr == None:
-                    curr = self.tail
-                else:
-                    curr = curr.prev
-                
-            if curr == None:
-                self.insert_element(prev_node=self.tail,new_node=new_node,next_node=curr)
-                return
-            self.insert_element(prev_node=curr.prev,new_node=new_node,next_node=curr)
-
-    
-    def delete_node(self,node):
-        if node.prev == None:
-            self.head = node.next
-        else:
-            node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def push(self,node):
-        new_node = Node(data=node.data,swapped=True)
-        self.insert_element_after_index(node,new_node=new_node)
-        self.delete_node(node)
-
-    def get_next_number(self):
-        curr = self.head
-        while curr.next != None:
-            if curr.swapped == False:
-                return curr
-            curr = curr.next
-        return None
-
-ll = LinkedList()
-
-for line in open('2022/20/t.txt'):
-    line = line.strip()
-    ll.append(int(line))
-ll.loop()
-for i in range(0,7):
-    next_number = ll.get_next_number()
-    if next_number.data != 0:
-        
-        ll.push(next_number)
+for k in x:
+    if k.n == 0:
+        #Setting the start node for later use
+        z = k
+        continue
+    p = k
+    if k.n > 0:
+        for _ in range(k.n):
+            p = p.right 
+        if k == p:
+            continue
+        #delete node
+        print(len(x))
+        k.right.left = k.left
+        k.left.right = k.right
+        print(len)
+        #add node
+        p.right.left = k
+        k.right = p.right
+        p.right = k
+        k.left = p
     else:
-        next_number.swapped = True
+        for _ in range(k.n):
 
-ll.print_list()
+            p = p.left
+        if k == p:
+            continue
+        k.left.right = k.right
+        k.right.left = k.left
+        p.left.right = k
+        k.left = p.left
+        p.left = k
+        k.right = p
+    break
+
+for k in x:
+    print(k.n)
+
+
