54ebded24c2c12322c22394a4b2ee990855ac521
comhendrik <hendrik0807@icloud.com>
2023-12-10 12:43:52 +0100

    day 10 part two but not finished 2023

--- 2023/10/ten.dart
+++ 2023/10/ten.dart
@@ -3,12 +3,14 @@ import 'dart:io';
 
 void main() async {
   List<List<String>> map = [];
+  List<(int,int)?> borders = [];
   var filePath = p.join(Directory.current.path, '2023/10/ten_input.txt');
   File file = File(filePath);
   var fileContent = await file.readAsLines();
   for (String line in fileContent) {
     final data = line.split('');
     map.add([]);
+    borders.add(null);
     for (String char in data) {
       if (char == 'S') {
       }
@@ -70,6 +72,58 @@ void main() async {
 
     count += 1;
 
+  }
+  print(count);
+
+  //these startings points for searched in the file without parsing it
+  (int,int) startLine = (49,95);
+  (int,int) prevStartLine = (49,96);
+  (int,int) start = (49,96);
+  Set<(int, int)> points = {
+    start,
+    startLine
+  };
+  print("Part two:");
+  while(true) {
+    if (startLine == start) break;
+    if(borders[startLine.$1] == null) {
+      borders[startLine.$1] = (startLine.$2, startLine.$2);
+    } else {
+      int min = borders[startLine.$1]!.$2;
+      int max = borders[startLine.$1]!.$2;
+      if (min > startLine.$2) {
+        min = startLine.$2;
+      }
+      if (max < startLine.$2) {
+        max = startLine.$2;
+      }
+      borders[startLine.$1] = (min, max);
+    }
+    String startingInstruction = map[startLine.$1][startLine.$2];
+    final coorsStartingOptionOne = instructionsOptionOne[startingInstruction]!;
+    final coorsStartingOptionTwo = instructionsOptionTwo[startingInstruction]!;
+    final newStartingPointOptionOne = (startLine.$1 + coorsStartingOptionOne.$1, startLine.$2 + coorsStartingOptionOne.$2);
+    final newStartingPointOptionTwo = (startLine.$1 + coorsStartingOptionTwo.$1, startLine.$2 + coorsStartingOptionTwo.$2);
+    if (newStartingPointOptionOne == prevStartLine) {
+      prevStartLine = startLine;
+      startLine = newStartingPointOptionTwo;
+    } else {
+      prevStartLine = startLine;
+      startLine = newStartingPointOptionOne;
+    }
+    points.add(startLine);
+
+  }
+
+  count = 0;
+  for (int i = 0; i<map.length; i++) {
+    for (int j = 0; j<map[i].length; j++) {
+      if (borders[i] != null && !points.contains((i,j))) {
+        if (j > borders[i]!.$1 && j < borders[i]!.$2) {
+          count += 1;
+        }
+      }
+    }
   }
   print(count);
 }
