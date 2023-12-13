27d0c513c1acaf02d6709fa7a862e50acfede41f
comhendrik <hendrik0807@icloud.com>
2023-12-13 19:43:58 +0100

    day 12 string generator 2023

--- 2023/12/twelve.dart
+++ 2023/12/twelve.dart
@@ -17,4 +17,45 @@ void main() async {
     }
   }
   print(damagedRecords);
+
+  String inputString = "???.###";
+  int n = 2;
+  List<String> options = generateOptions(inputString, n);
+  print(options);
+}
+
+
+
+List<String> generateOptions(String inputStr, int n) {
+  List<String> result = [];
+
+  void backtrack(int index, int remaining, List<String> current) {
+    if (index == inputStr.length) {
+      if (remaining == 0) {
+        result.add(current.join());
+      }
+      return;
+    }
+
+    if (inputStr[index] == '?' && remaining > 0) {
+      current[index] = '#';
+      backtrack(index + 1, remaining - 1, List.from(current));
+    }
+
+    current[index] = inputStr[index];
+    backtrack(index + 1, remaining, List.from(current));
+  }
+
+  backtrack(0, n, List.filled(inputStr.length, ''));
+
+  for (int i=0; i<result.length; i++) {
+    List<String> chars = result[i].split('');
+    for (int j=0; j<chars.length; j++) {
+      if (chars[j] == '?') {
+        chars[j] = '.';
+      }
+    }
+    result[i] = chars.join();
+  }
+  return result;
 }
