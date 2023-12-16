ec49e899c844646f39791901bd5976a78220ed03
comhendrik <hendrik0807@icloud.com>
2023-12-16 19:16:45 +0100

    started day 16 part one 2023

---
+++ 2023/16/sixteen.dart
@@ -0,0 +1,92 @@
+import 'package:path/path.dart' as p;
+import 'dart:io';
+
+void main() async {
+  List<List<String>> map = [];
+  var filePath = p.join(Directory.current.path, '2023/16/sixteen_input.txt');
+  File file = File(filePath);
+  var fileContent = await file.readAsLines();
+  for (String line in fileContent) {
+    final data = line.split('');
+    map.add([]);
+    for (String char in data) {
+      map.last.add(char);
+    }
+  }
+  print("Part one:");
+  //first one is the current coordination of beam, second is the current movement direction
+  List<((int, int), (int, int))> beams = [((0,0),(0,1))];
+  //second list to add new beams to list and the copy them to the next List
+  List<((int, int), (int, int))> newBeams = [];
+  //Set to check which points of the map where visited;
+  Set<(int,int)> visited = {(0,0)};
+  int sum = 0;
+  while (!beams.isEmpty) {
+    for (((int, int), (int, int)) beam in beams) {
+      (int, int) newCoors = (beam.$1.$1 + beam.$2.$1, beam.$1.$2 + beam.$2.$2);
+      if(newCoors.$1 < 0 || newCoors.$2 < 0) continue;
+      if(newCoors.$1 >= map.length || newCoors.$2 >= map.first.length) continue;
+      ((int, int),(int, int)?) newDirection = checkNewDirection(beam.$2, map[newCoors.$1][newCoors.$2]);
+      newBeams.add((newCoors, newDirection.$1));
+      if (newDirection.$2 != null) newBeams.add((newCoors, newDirection.$2!));
+      visited.add(newCoors);
+    }
+    beams = newBeams;
+    print(newBeams.length);
+    newBeams = [];
+    sum += 1;
+  }
+}
+
+((int, int), (int, int)?) checkNewDirection((int,int) currentDirection, String char) {
+  final y = currentDirection.$1;
+  final x = currentDirection.$2;
+  //only four options:
+  //y=0 x=1
+  //y=0 x=-1
+  //y=1 x=0
+  //y=-1 x=0
+  //based on the char and currentDirection, the new direction is created
+  if(y == 0) {
+    if(x==1) {
+      //y=0 x=1
+      switch (char) {
+        case '|': return ((-1,0),(1,0));
+        case '-': return ((0,1), null);
+        case '.': return ((0,1),null);
+        case '/': return ((-1,0), null);
+        case '\\': return ((1,0), null);
+      }
+    } else {
+      //y=0 x=-1
+      switch (char) {
+        case '|': return ((-1,0),(1,0));
+        case '-': return ((0,-1), null);
+        case '.': return ((0,-1),null);
+        case '/': return ((1,0), null);
+        case '\\': return ((-1,0), null);
+      }
+    }
+  } else if(x==0){
+    if(y==1) {
+      //y=1 x=0
+      switch (char) {
+        case '|': return ((1,0),null);
+        case '-': return ((0,-1), (0,1));
+        case '.': return ((1,0),null);
+        case '/': return ((0,-1), null);
+        case '\\': return ((0,1), null);
+      }
+    } else {
+      //y=-1 x=0
+      switch (char) {
+        case '|': return ((-1,0),null);
+        case '-': return ((0,1), (0,-1));
+        case '.': return ((-1,0),null);
+        case '/': return ((0,1), null);
+        case '\\': return ((0,-1), null);
+      }
+    }
+  }
+  return ((0,0),null);
+}
