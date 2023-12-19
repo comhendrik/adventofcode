049f0d00194e1d98c1624c1d76e8e4d7d4278fb7
comhendrik <hendrik0807@icloud.com>
2023-12-19 20:16:47 +0100

    completed day 18 part one 2023

--- 2023/18/eighteen.dart
+++ 2023/18/eighteen.dart
@@ -48,13 +48,8 @@ void main() async {
     map[coors.$1 + addingY][coors.$2 +addingX] = '#';
   }
 
-
-  for (List<String> line in map){
-    print(line);
-  }
-  //add method to get edgepoints
-
-  List<(int,int)> edgePoints = [(2,0), (0,0), (0,6), (5,6), (5,4), (7,4), (7,6), (9,6), (9,1), (7,1), (7,0), (5,0), (5,2), (2,2), (2,0), (0,0)];
+  List<(int,int)> edgePoints = getCornerPoints(map, coordinates, addingY, addingX);
+  edgePoints.insert(0, edgePoints[edgePoints.length - 2]);
 
   double area = 0;
   //calculating area by using shoelace formula and picks theorem
@@ -70,45 +65,41 @@ void main() async {
 
   if (area < 0) area * -1;
   area = area / 2;
-  print(area);
   //starting of picks theorem
   int b = coordinates.length - 1;
   double i = area - (b / 2) + 1;
   // b are interior points and i outside of the polygon
   print(i+b);
   return;
+}
 
-  List<(int,int)> coordinatesToAdd = [];
+List<(int,int)> getCornerPoints(List<List<String>> polygon, List<(int,int)> coordinates, int addingY, int addingX) {
+  List<(int,int)> cornerPoints = [];
 
-  for (int i=0; i<map.length; i++) {
-    for (int j=0; j<map.first.length; j++) {
-      if (map[i][j] == '#') continue;
-      int crossings = 0;
-      bool isInLine = false;
-      for(int z=j-1; z>=0; z--) {
-        if (map[i][z] == '#' && isInLine == false) {
-          crossings += 1;
-          isInLine = true;
-        } else if (map[i][z] != '#' && isInLine == true) {
-          isInLine = false;
-        }
+  for((int,int) coordinate in coordinates) {
+    int i = coordinate.$1 + addingY;
+    int j = coordinate.$2 + addingX;
+    if (polygon[i][j] == "#") {
+      // Check if the current point is a corner by checking for adjacent '#' characters
+      bool isCorner = false;
+      if (i > 0 && j > 0 && polygon[i - 1][j] == "#" && polygon[i][j - 1] == "#") {
+        isCorner = true;
+      }
+      if (i > 0 && j < polygon[i].length - 1 && polygon[i - 1][j] == "#" && polygon[i][j + 1] == "#") {
+        isCorner = true;
+      }
+      if (i < polygon.length - 1 && j > 0 && polygon[i + 1][j] == "#" && polygon[i][j - 1] == "#") {
+        isCorner = true;
+      }
+      if (i < polygon.length - 1 && j < polygon[i].length - 1 && polygon[i + 1][j] == "#" && polygon[i][j + 1] == "#") {
+        isCorner = true;
       }
 
-      if (crossings % 2 == 1) {
-        coordinatesToAdd.add((i,j));
+      if (isCorner) {
+        cornerPoints.add((i, j)); // Tuple2 from the 'tuple' package
       }
     }
   }
 
-  for ((int,int) coors in coordinatesToAdd) {
-    map[coors.$1][coors.$2] = '#';
-  }
-
-  int sum = 0;
-  for (int i=0; i<map.length; i++) {
-    for (int j=0; j<map.first.length; j++) {
-      if (map[i][j] == '#') sum++;
-    }
-  }
-  print(sum);
+  return cornerPoints;
 }
