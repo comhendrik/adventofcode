a4f11529dc6b07014dff729eb9fab104e16094f6
comhendrik <hendrik0807@icloud.com>
2023-12-21 17:02:11 +0100

    completed day 19 part one 2023

--- 2023/19/nineteen.dart
+++ 2023/19/nineteen.dart
@@ -1,26 +1,33 @@
-void main() {
+import 'package:path/path.dart' as p;
+import 'dart:io';
 
-  List<Part> parts = [
-    Part(787,2655,1222,2876),
-    Part(1679,44,2067,496),
-    Part(2036,264,79,2244),
-    Part(2461,1339,466,291),
-    Part(2127,1623,2188,1013),
-  ];
+void main() async {
+  var filePath = p.join(Directory.current.path, '2023/19/nineteen_input.txt');
+  List<Part> parts = [];
+  Map<String, List<String>> workflows = {};
+  bool addToParts = true;
+  File file = File(filePath);
+  var fileContent = await file.readAsLines();
+  for (String line in fileContent) {
+    if (line == "") {
+      addToParts = false;
+      continue;
+    }
+    if(addToParts) {
+      final data = line.split('{');
+      final rules = data[1].substring(0,data[1].length -1).split(',');
+      workflows[data[0]] = rules;
+    } else {
+      final data = line.split(',');
+      final x = int.parse(data[0].substring(3, data[0].length));
+      final m = int.parse(data[1].substring(2, data[1].length));
+      final a = int.parse(data[2].substring(2, data[2].length));
+      final s = int.parse(data[3].substring(2, data[3].length-1));
+      parts.add(Part(x, m, a, s));
+    }
+  }
 
-  Map<String, List<String>> workflows = {
-    'px' : ['a<2006:qkq','m>2090:A','rfg'],
-    'pv' : ['a>1716:R','A'],
-    'lnx' : ['m>1548:A','A'],
-    'rfg' : ['s<537:gd','x>2440:R','A'],
-    'qs' : ['s>3448:A','lnx'],
-    'qkq' : ['x<1416:A','crn'],
-    'crn' : ['x>2662:A','R'],
-    'in' : ['s<1351:px','qqz'],
-    'qqz' : ['s>2770:qs','m<1801:hdj','R'],
-    'gd' : ['a>3333:R','R'],
-    'hdj' : ['m>838:A','pv']
-  };
+  print("Part one:");
 
   for(Part part in parts) {
     bool ended = false;
@@ -31,7 +38,6 @@ void main() {
           ended = true;
           break;
         } else if (rule == 'R') {
-          print("yeah2");
           part.rejected = true;
           ended = true;
           break;
@@ -41,7 +47,7 @@ void main() {
         }
         final splittedRule = rule.split(':');
         final comparisonValueOfPart = part.accessMap[splittedRule[0][0]]!;
-        final comparisonValue = int.parse(splittedRule[0].substring(2, splittedRule[0].length-1));
+        final comparisonValue = int.parse(splittedRule[0].substring(2, splittedRule[0].length));
         final action = splittedRule[1];
         final operator = splittedRule[0][1];
         bool successful = false;
@@ -50,7 +56,6 @@ void main() {
         } else {
           if (comparisonValueOfPart < comparisonValue)  successful = true;
         }
-
         if(!successful) continue;
 
         switch (action) {
@@ -58,7 +63,6 @@ void main() {
             ended = true;
           case 'R' :
             part.rejected = true;
-            print("yeah1");
             ended = true;
           default:
             currentWorkflow = workflows[action]!;
@@ -68,9 +72,12 @@ void main() {
     }
   }
 
+  int sum = 0;
   for (Part part in parts) {
-    print(part.rejected);
+    if (part.rejected) continue;
+    sum += (part.x + part.m + part.a + part.s);
   }
+  print(sum);
 }
 
 class Part {
