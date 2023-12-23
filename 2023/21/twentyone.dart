94963d7c350f97a014e0b78ac0d7a4c84aa28c62
comhendrik <hendrik0807@icloud.com>
2023-12-23 09:50:40 +0100

    day 21 part two works but not for large numbers 2023

--- 2023/21/twentyone.dart
+++ 2023/21/twentyone.dart
@@ -7,17 +7,19 @@ void main() async {
   var fileContent = await file.readAsLines();
   List<List<String>> map = [];
   Set<(int,int)> points = {};
+  (int,int) startingPoint = (0,0);
   //read map
   for (String line in fileContent) {
     map.add([]);
     for (String char in line.split('')) {
       map.last.add(char);
       if (char == 'S') {
-        points.add((map.length-1, map.last.length-1));
+        startingPoint = (map.length-1, map.last.length-1);
       }
     }
   }
 
+  points.add(startingPoint);
   print("Part one");
   //for n amount of steps two the movement
   for(int i=0; i<64; i++) {
@@ -37,6 +39,31 @@ void main() async {
   }
   //after doing all steps you can count the number of positions
   print(points.length);
+
+
+  //reloading everything
+  points = {};
+  points.add(startingPoint);
+
+  print("Part two");
+  //for n amount of steps two the movement
+  for(int i=0; i<10; i++) {
+    Set<(int,int)> newPoints = {};
+    for((int,int) point in points) {
+      final north = generateNewPointsP2(point, (-1,0), map);
+      final east = generateNewPointsP2(point, (0,1), map);
+      final south = generateNewPointsP2(point, (1,0), map);
+      final west = generateNewPointsP2(point, (0,-1), map);
+      if (north != null) newPoints.add(north);
+      if (east != null) newPoints.add(east);
+      if (south != null) newPoints.add(south);
+      if (west != null) newPoints.add(west);
+    }
+    points = newPoints;
+  }
+  //after doing all steps you can count the number of positions
+  print(points.length);
+
 }
 
 (int,int)? generateNewPoints((int,int) currentPoint, (int,int) direction, List<List<String>> map) {
@@ -46,6 +73,19 @@ void main() async {
   final maxX = map.first.length - 1;
   if (y<0 || y > maxY) return null;
   if (x<0 || x > maxX) return null;
+  if (map[y][x] == '#') return null;
+  return (y,x);
+}
+
+(int,int)? generateNewPointsP2((int,int) currentPoint, (int,int) direction, List<List<String>> map) {
+  int y = currentPoint.$1 + direction.$1;
+  int x = currentPoint.$2 + direction.$2;
+  final maxY = map.length - 1;
+  final maxX = map.first.length - 1;
+  if (y > maxY) y = 0;
+  if (y < 0) y = maxY;
+  if (x > maxX) x = 0;
+  if (x < 0) x = maxX;
   if (map[y][x] == '#') return null;
   return (y,x);
 }
