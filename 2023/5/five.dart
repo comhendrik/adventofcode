8f9f4e2a7e8e0631902efda29eb7f83cfcab3639
comhendrik <hendrik0807@icloud.com>
2023-12-25 09:49:02 +0100

    completed day 5 part two, but not for large inputs 2023

--- 2023/5/five.dart
+++ 2023/5/five.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 void main() async {
 
   List<int> seeds = [];
+  List<int> seedsP2 = [];
   List<List<(int,int,int)>> maps = [];
   var filePath = p.join(Directory.current.path, '2023/5/five_input.txt');
   File file = File(filePath);
@@ -13,7 +14,13 @@ void main() async {
     if(line.contains('seeds:')) {
       final data = line.split(' ');
       for(int i=1; i<data.length; i++) {
-        seeds.add(int.parse(data[i]));
+        final value = int.parse(data[i]);
+        seeds.add(value);
+        if(i%2!=0) {
+          for(int j=0; j<int.parse(data[i+1]); j++) {
+            seedsP2.add(value+j);
+          }
+        }
       }
     } else if(line.contains(':')) {
       maps.add([]);
@@ -24,7 +31,7 @@ void main() async {
 
   }
 
-
+  print(seedsP2.length);
 
   print("Part one:");
   List<int> finalSeeds = [];
@@ -33,7 +40,6 @@ void main() async {
   for (int seed in seeds) {
     int currentSeedNumber = seed;
     for(List<(int,int,int)> map in maps) {
-      bool foundNewValue = false;
       for ((int,int,int) line in map) {
         int destinationRangeStart = line.$1;
         int sourceRangeStart = line.$2;
@@ -43,15 +49,40 @@ void main() async {
         if (sourceRangeStart <= currentSeedNumber && currentSeedNumber <= sourceRangeStart + rangeLength) {
           final differenceToAdd = currentSeedNumber - sourceRangeStart;
           currentSeedNumber = destinationRangeStart + differenceToAdd;
-          foundNewValue = true;
           break;
         }
       }
-      if (foundNewValue) continue;
     }
     finalSeeds.add(currentSeedNumber);
   }
 
   finalSeeds.sort();
   print(finalSeeds.first);
+
+
+  print("part two, works but not for large inputs");
+  List<int> finalSeedsP2 = [];
+
+
+  for (int seed in seedsP2) {
+    int currentSeedNumber = seed;
+    for(List<(int,int,int)> map in maps) {
+      for ((int,int,int) line in map) {
+        int destinationRangeStart = line.$1;
+        int sourceRangeStart = line.$2;
+        int rangeLength = line.$3;
+
+        //check if seed needs to be computed here, need to be computed when its in source range
+        if (sourceRangeStart <= currentSeedNumber && currentSeedNumber <= sourceRangeStart + rangeLength) {
+          final differenceToAdd = currentSeedNumber - sourceRangeStart;
+          currentSeedNumber = destinationRangeStart + differenceToAdd;
+          break;
+        }
+      }
+    }
+    finalSeedsP2.add(currentSeedNumber);
+  }
+
+  finalSeedsP2.sort();
+  print(finalSeedsP2.first);
 }
